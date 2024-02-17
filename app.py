8b29bef8cdb48f83bebeae429801227c89f6ccc9
gbemike <gbemike2002@gmail.com>
2024-02-17 23:07:27 +0000

    stage

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ from langchain.chains import (
     StuffDocumentsChain, LLMChain, ConversationalRetrievalChain
 )
 
-from langchain.chains import create_qa_with_sources_chain
 from langchain_community.vectorstores.chroma import Chroma
 from PyPDF2 import PdfReader
 
@@ -45,31 +44,31 @@ if 'requests' not in st.session_state:
     st.session_state['requests'] = []
 
 if 'buffer_memory' not in st.session_state:
-            st.session_state.buffer_memory=ConversationBufferWindowMemory(memory_key='history', human_prefix = 'Human', ai_prefix= 'AI', k=3,return_messages=True)
+            st.session_state.buffer_memory=ConversationBufferWindowMemory(memory_key='history',
+                                                                          k=3,
+                                                                          return_messages=True)
+            
+st.write(st.session_state)
 
-embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-db = Chroma('chroma_db',embeddings)
-
-template = """ You are a python language expert , 
-                always introduce yourself as such when, 
+template = """  Make sure you remember the users name and always acknowledge it,
+                You are a python language expert, 
+                always introduce yourself as such, 
                 you are to provide code solutions to all python related questions
-                Current Conversation:
+                Chat_history:
                 {history}
-                Python AI Assistant: """
-
-#PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
+                Current Conversation:
+                """
 
 system_msg_template = SystemMessagePromptTemplate.from_template(template=template)
-
 
 human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 
 prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
 
 conversation = ConversationChain(
-     memory=st.session_state.buffer_memory, 
-     prompt=prompt_template, 
-     llm=llm, 
+     memory=st.session_state.buffer_memory,
+     prompt=prompt_template,
+     llm=llm,
      verbose=True)
 
 # container for chat history
@@ -79,11 +78,10 @@ response_container = st.container()
 query_container = st.container()
 
 with query_container:
-    query = st.text_input('Query: ')
+    query = st.text_input('Query: ', key='input')
     if query:
         with st.spinner("typing...."):
-            context = db.similarity_search(query=query)
-            response = conversation.predict(input=f"\n {context} \n Human: {query}")
+            response = conversation.predict(input=f"\n{query}\n")
         st.session_state.requests.append(query)
         st.session_state.responses.append(response)
 
